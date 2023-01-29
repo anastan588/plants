437d1e4d6be1a9fae939cfc784c9a13ee367eeb5
anastan588 <anastasiya5.88@mail.ru>
2023-01-29 18:53:57 +0300

    feat: edit focus on cards

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ let overLay = document.querySelector('.overlay');
 let buttonsService =document.querySelectorAll(".button_service");
 let cards = document.querySelectorAll(".service_card");
 let nameOfCards = document.querySelectorAll(".card_name");
+let gardenButton = document.querySelector(".garden");
+let lawnButton = document.querySelector(".lawn");
+let plantButton = document.querySelector(".plant");
+
+
 console.log(buttonsService);
 console.log(cards);
 console.log(nameOfCards);
@@ -28,6 +33,8 @@ function burgerClose() {
     overLay.style.display="none";
 }
 
+
+
 function focusCard(eventButton) {
   
    if (eventButton.innerHTML[0] ==="G") {
@@ -35,14 +42,16 @@ function focusCard(eventButton) {
     for (let j=0; j<nameOfCardsArray.length; j++) {
       console.log(nameOfCardsArray[j].innerHTML[0]);
       if (nameOfCardsArray[j].innerHTML[0]===eventButton.innerHTML[0]) {
-        cards[j].classList.add("service_focus");
-        //cards[j].style.transform = "scale(1.05)";
+        cards[j].classList.add("service_focus"); 
+        cards[j].classList.remove("service_nonefocus");       
         console.log(cards[j].className);
         } else if (nameOfCardsArray[j].innerHTML[0]!== eventButton.innerHTML[0]) {
-          cards[j].classList.add("service_nonefocus");
-          //cards[j].style.filter = "blur(10px)";
-        }
-       
+          if (cards[j].classList.contains("service_focus"))  {
+            cards[j].classList.remove("service_nonefocus");
+          } else {
+            cards[j].classList.add("service_nonefocus");
+          }
+          } 
       }
     }
     if (eventButton.innerHTML[0] ==="L") {
@@ -51,11 +60,16 @@ function focusCard(eventButton) {
         console.log(nameOfCardsArray[j].innerHTML[0]);
         if (nameOfCardsArray[j].innerHTML[0]===eventButton.innerHTML[0]) {
           cards[j].classList.add("service_focus");
-          //cards[j].style.transform = "scale(1.05)";
+          cards[j].classList.remove("service_nonefocus"); 
           console.log(cards[j].className);
-          } else if (nameOfCardsArray[j].innerHTML[0]!== eventButton.innerHTML[0]) {
-            cards[j].classList.add("service_nonefocus");
-            //cards[j].style.filter = "blur(10px)";
+          } else if (nameOfCardsArray[j].innerHTML[0]!== eventButton.innerHTML[0] ) {
+            if (cards[j].classList.contains("service_focus"))  {
+              cards[j].classList.remove("service_nonefocus");
+            } else {
+              cards[j].classList.add("service_nonefocus");
+            }
+                   
+          
           }
          
         }
@@ -66,11 +80,17 @@ function focusCard(eventButton) {
           console.log(nameOfCardsArray[j].innerHTML[0]);
           if (nameOfCardsArray[j].innerHTML[0]===eventButton.innerHTML[0]) {
             cards[j].classList.add("service_focus");
-            //cards[j].style.transform = "scale(1.05)";
+            cards[j].classList.remove("service_nonefocus"); 
+            
             console.log(cards[j].className);
             } else if (nameOfCardsArray[j].innerHTML[0]!== eventButton.innerHTML[0]) {
-              cards[j].classList.add("service_nonefocus");
-              //cards[j].style.filter = "blur(10px)";
+              if (cards[j].classList.contains("service_focus"))  {
+                cards[j].classList.remove("service_nonefocus");
+              } else {
+                cards[j].classList.add("service_nonefocus");
+              }
+
+             
             }
            
           }
@@ -134,20 +154,36 @@ burgerCloseButton.addEventListener("click",burgerClose);
 burgerMenulink.forEach( (element) => element.addEventListener("click", burgerClose));
 overLay.addEventListener("click", burgerClose);
 var event;
+
 function add(event) {
-  event.preventDefault();
+  /*for (let i =0; i<buttonsService.length; i++) {
+      buttonsService[i].classList.toggle(".active");
+    }*/
+   
+   
   let eventButton = event.target;
+  /*if(eventButton.classList.contains(".active")) {
+    return eventButton.classList.remove(".active");
+    
+  }*/
+  eventButton.classList.add(".active");
   console.log(eventButton);
   focusCard(eventButton);
 };
 
+
 function remove(event) {
   let eventButton = event.target;
-  
+  eventButton.classList.remove(".active");
+
   console.log(eventButton);
   blurCard(eventButton);
 };
 
-buttonsService.forEach((element) => element.addEventListener("focus", add));
-buttonsService.forEach((element) => element.addEventListener("blur", remove));
+
+//gardenButton.addEventListener("click", add);
+//lawnButton.addEventListener("click", add);
+//plantButton.addEventListener("click", add);
+buttonsService.forEach((element) => element.addEventListener("click", add));
+//buttonsService.forEach((element) => element.addEventListener("blur", remove));
 
